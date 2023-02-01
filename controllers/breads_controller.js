1834cd6bd440a8edc6c218025c9ca21cde4f6d11
Jorge Vargas <jorgevargas@jorges-mbp.mynetworksettings.com>
2023-02-01 14:59:30 -0500

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,18 +32,24 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
-
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    if (Bread[req.params.arrayIndex]) {
-      res.render('Show', {
-        bread:Bread[req.params.arrayIndex]
-      })
-    } else {
-      res.render('404')
-    }
-  })
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
+    })
+  } else {
+    res.render('404')
+  }
+})
+
 
   
   
--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread}) {
+function Show ({bread, index}) {
     return (
         <Default>
             <h2>Show Page</h2>
@@ -19,6 +19,12 @@ function Show ({bread}) {
             <li>
                 <a href="/breads">Go home</a>
             </li>
+
+            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+                <input type='submit' value="DELETE"/>
+            </form>
+
+
         </Default>
     )
 }
